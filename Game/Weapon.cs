11e0d2d7cba432a8f54bfe05d0598917b96549b0
algor1 <alexeev.igor.v@gmail.com>
2019-03-17 00:28:00 +0300

    no message

--- Game/Weapon.cs
+++ Game/Weapon.cs
@@ -26,6 +26,7 @@ namespace SpaceObjects
             host = _host;
             //activated = false;
             fire = false;
+            Reload();
         }
         public void BeforeDestroy()
         {
@@ -59,6 +60,7 @@ namespace SpaceObjects
             mineTarget = null;
             fire = false;
             mine = false;
+            Reload();
         }
 
         private async Task Reload()
